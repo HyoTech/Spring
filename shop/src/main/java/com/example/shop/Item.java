e46406d94e2f092688e5c35d7c947001b32aa6b6
HyoTech <gyxordl08@naver.com>
2024-07-25 00:36:31 +0900

    20240725FirstPush

---
+++ shop/src/main/java/com/example/shop/Item.java
@@ -0,0 +1,19 @@
+package com.example.shop;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Item {
+    // @Id = Public key 설정, GenerateValue = auto increment 기능/ 컬럼 생성
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long ID;
+
+    @Column(length = 200)
+    public String ProductName;
+    public Integer Price;
+}
