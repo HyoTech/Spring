05a54f8ec5bbe9b3f6b82466aa8a9b212b836968
HyoTech <gyxordl08@naver.com>
2024-07-28 16:19:41 +0900

    20240728

---
+++ shop/src/main/java/com/example/shop/InfoRepository.java
@@ -0,0 +1,7 @@
+package com.example.shop;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InfoRepository extends JpaRepository<Information, Long> {
+
+}
--- shop/src/main/java/com/example/shop/ItemController.java
+++ shop/src/main/java/com/example/shop/ItemController.java
@@ -3,16 +3,25 @@ package com.example.shop;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.*;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+    private final ItemRepository itemRepository;
+    private final InfoRepository infoRepository;
 
-    // 1. 서버 API 함수의 파라미터에 Model model 넣고
-    // 2. API안에서 model.addAttribute("작명", 전송할데이터)
-    // 3. html 태그에 th:text="${작명}"
     @GetMapping("/list")
     String list(Model model) {
-        model.addAttribute("name", "홍길동");
+        List<Item> ItemResult = itemRepository.findAll();
+        List<Information> InfoResult = infoRepository.findAll();
+        // 1. 서버 API 함수의 파라미터에 Model model 넣고
+        // 2. API안에서 model.addAttribute("작명", 전송할데이터)
+        // 3. html 태그에 th:text="${작명}"
+        model.addAttribute("items", ItemResult);
+        model.addAttribute("infos", InfoResult);
         return "list.html";
     }
 }
