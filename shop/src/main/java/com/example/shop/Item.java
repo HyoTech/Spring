ffd9e84b5925b2b857e2b6b9fdbaedbe70a00d82
HyoTech <gyxordl08@naver.com>
2024-07-29 17:11:19 +0900

    20240729

--- shop/src/main/java/com/example/shop/Item.java
+++ shop/src/main/java/com/example/shop/Item.java
@@ -11,9 +11,38 @@ public class Item {
     // @Id = Public key 설정, GenerateValue = auto increment 기능/ 컬럼 생성
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public long ID;
+    private long ID;
 
     @Column(length = 200)
-    public String ProductName;
-    public Integer Price;
+    private String ProductName;
+    private Integer Price;
+
+    public String getProductName() {
+        return ProductName;
+    }
+
+    public Integer getPrice() {
+        return Price;
+    }
+
+    public void setProductName(String ProductName) {
+        this.ProductName = ProductName;
+    }
+
+    public void setPrice(Integer Price) {
+        this.Price = Price;
+    }
+    /*
+     * public void setAge(Integer Age) {
+     * if (Age <= 100 && 0 <= Age) {
+     * this.Age = Age;
+     * } else {
+     * Age = Age;
+     * }
+     * }
+     * 
+     * public void PlusAge() {
+     * this.Age = Age + 1;
+     * }
+     */
 }
