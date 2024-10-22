e018ed9b2ba1843aa3ed87a7cc356cd0cf6c8440
HyoTech <gyxordl08@naver.com>
2024-10-22 18:59:02 +0900

    공지조회수추가

--- shop/src/main/java/com/example/shop/Infomation/InfoController.java
+++ shop/src/main/java/com/example/shop/Infomation/InfoController.java
@@ -2,6 +2,7 @@ package com.example.shop.Infomation;
 
 import java.util.*;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -22,25 +24,40 @@ public class InfoController {
     }
 
     @PostMapping("/SubmitInfo")
-    public String postMethodName(@RequestParam("InfoTitle") String InfoTilte, @RequestParam("Writer") String Writer,
+    public String savedInfo(@RequestParam("InfoTitle") String InfoTilte, @RequestParam("Writer") String Writer,
             @RequestParam("InfoValue") String InfoValue, @RequestParam("InfoDate") String InfoDate) {
         Information info = new Information();
         info.setInfo_Date(InfoDate);
         info.setInfo_Title(InfoTilte);
         info.setInfo_Value(InfoValue);
         info.setWriter(Writer);
+        info.setView(0);
         infoRepository.save(info);
         return "Info.html";
     }
 
     @GetMapping("/detailInfo/{id}")
-    public String postMethodName(@PathVariable("id") long id, Model model) {
+    public String detailInfo(@PathVariable("id") long id, Model model) {
         Optional<Information> Result = infoRepository.findById(id);
 
         if (Result.isPresent()) {
             model.addAttribute("dInfo", Result.get());
         }
         return "detailInfo.html";
+    }
+
+    @PostMapping("/countClick/{id}")
+    ResponseEntity<String> plusViewCount(@PathVariable("id") long id, @RequestBody Map<String, Integer> body) {
+        Optional<Information> result = infoRepository.findById(id);
+
+        if (result.isPresent()) {
+            Information info = result.get();
+            Integer view = body.get("View");
+            info.setView(view);
+            infoRepository.save(info);
+        }
+
+        return ResponseEntity.status(200).body("Count");
     }
 
 }
