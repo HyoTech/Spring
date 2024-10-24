d262274edeb4424b38ae00cb781375a166ee5067
HyoTech <gyxordl08@naver.com>
2024-10-24 23:33:47 +0900

    공지사항D추가

--- shop/src/main/java/com/example/shop/Infomation/Information.java
+++ shop/src/main/java/com/example/shop/Infomation/Information.java
@@ -1,5 +1,6 @@
 package com.example.shop.Infomation;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +18,8 @@ public class Information {
 
     public String Info_Title;
     public String Info_Value;
+
+    @Column(name = "writer")
     public String Writer;
     public String Info_Date;
     public Integer View;
